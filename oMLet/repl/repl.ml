7572a40e357edb8a5a9eca295df58aeed07fa9e5
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-10-04 21:34:32 +0300

    feat: launch of compiler inside repl
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- oMLet/repl/repl.ml
+++ oMLet/repl/repl.ml
@@ -4,6 +4,8 @@
 
 open OMLet.Ast
 open OMLet.Parser
+open OMLet.Codegen
+open OMLet.CodegenTypes
 open Base
 open Stdio
 
@@ -32,19 +34,16 @@ let run_single dump_parsetree stop_after eval input_source =
   | Error e -> Stdlib.Format.printf "Parsing error: %s\n%!" e
   | Ok ast ->
     if dump_parsetree then print_endline (show_constructions ast);
+    let instructions = codegen ast in
+    let _ = Format.fprintf Format.std_formatter ".global _start\n" in
+    let _ = Stdlib.List.iter pp_instr instructions in
     (match stop_after with
      | SA_parsing -> ()
-     | SA_never ->
-       eval ast)
+     | SA_never -> eval ast)
 ;;
 
 let () =
-  let opts =
-    { dump_parsetree = false
-    ; stop_after = SA_never
-    ; input_file = None
-    }
-  in
+  let opts = { dump_parsetree = false; stop_after = SA_never; input_file = None } in
   let () =
     Stdlib.Arg.parse
       [ ( "-dparsetree"
@@ -61,13 +60,9 @@ let () =
         , "Read code from the specified file" )
       ]
       (fun _ ->
-        Stdlib.Format.eprintf "Positional arguments are not supported\n";
-        Stdlib.exit 1)
+         Stdlib.Format.eprintf "Positional arguments are not supported\n";
+         Stdlib.exit 1)
       "Read-Eval-Print-Loop for custom language"
   in
-  run_single
-    opts.dump_parsetree
-    opts.stop_after
-    (fun ast -> eval ast)
-    opts.input_file
+  run_single opts.dump_parsetree opts.stop_after (fun ast -> eval ast) opts.input_file
 ;;
