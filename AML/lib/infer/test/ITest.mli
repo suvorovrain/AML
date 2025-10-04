c2b694737ed76b0081ca5777145e11c31d7a1ce3
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-04 19:32:32 +0300

    feat: mli files
    This patch adds mli files for new modules.
    Also there is a few changes that was made due
    to zanuda linter report.
    
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

---
+++ AML/lib/infer/test/ITest.mli
@@ -0,0 +1,7 @@
+(** Copyright 2025-2026, Rodion Suvorov, Dmitriy Chirkov*)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Inferencer.Infer
+
+val pprint_result : TypeEnv.t -> string list -> unit
