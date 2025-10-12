bcadf284b135e3856d12f98e6f95c4560ae0634e
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-12 23:26:10 +0300

    feat: implement anf transformer for expressions
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- AML/lib/middleend/anf.ml
+++ AML/lib/middleend/anf.ml
@@ -23,6 +23,7 @@ type cexpr =
   | CBinop of binop * immexpr * immexpr
   | CApp of immexpr * immexpr list
   | CIte of immexpr * aexpr * aexpr
+  | CFun of ident * aexpr
 
 and aexpr =
   | ACE of cexpr
@@ -79,14 +80,48 @@ let define_binop = function
   | s -> failwith ("unsupported binop: " ^ s)
 ;;
 
-let rec transform_expr : Expression.t -> (immexpr -> aexpr ANFState.t) -> aexpr ANFState.t
+let get_pattern_name =
+  fun pat ->
+  match pat with
+  | Pattern.Pat_var name -> name
+  | _ -> failwith "not supported"
+;;
+
+let extract_cexpr aexpr =
+  match aexpr with
+  | ACE cexpr -> cexpr
+  | _ -> failwith "unreachable"
+;;
+
+let rec transform_list
+  : Expression.t list -> (immexpr list -> aexpr ANFState.t) -> aexpr ANFState.t
   =
+  fun l k ->
+  match l with
+  | hd :: tl ->
+    transform_expr hd (fun res_hd ->
+      transform_list tl (fun res_tl -> k (res_hd :: res_tl)))
+  | [] -> k []
+
+and transform_expr : Expression.t -> (immexpr -> aexpr ANFState.t) -> aexpr ANFState.t =
   fun expr k ->
   match expr with
   | Exp_constant (Constant.Const_integer exp) -> k @@ ImmNum exp
   | Exp_ident exp -> k @@ ImmId exp
-  (* | Exp_apply (Exp_ident op, Exp_tuple (exp1, exp2, [])) when is_binop op -> failwith "todo" *)
-  (* | Exp_apply (exp1, exp2) -> failwith "todo" *)
+  | Exp_apply (Exp_ident op, Exp_tuple (exp1, exp2, [])) when is_binop op ->
+    transform_expr exp1 (fun exp1_res ->
+      transform_expr exp2 (fun exp2_res ->
+        let* t = fresh_temp in
+        let* rest = k @@ ImmId t in
+        return
+        @@ ALet (Nonrecursive, t, CBinop (define_binop op, exp1_res, exp2_res), rest)))
+  | Exp_apply (exp1, exp2) ->
+    let args, fn = app_args_to_list @@ Exp_apply (exp1, exp2) in
+    transform_expr fn (fun fn_res ->
+      transform_list args (fun args_res ->
+        let* t = fresh_temp in
+        let* rest = k @@ ImmId t in
+        return @@ ALet (Nonrecursive, t, CApp (fn_res, args_res), rest)))
   | Exp_let (flag, ({ pat = Pattern.Pat_var pat; expr }, _), exp) ->
     transform_expr expr (fun a ->
       let* res = transform_expr exp k in
@@ -96,12 +131,21 @@ let rec transform_expr : Expression.t -> (immexpr -> aexpr ANFState.t) -> aexpr 
       let* then_res = transform_expr then_expr k in
       let* else_res = transform_expr else_expr k in
       return (ACE (CIte (cond_res, then_res, else_res))))
-  (* | Exp_fun (exp1, exp2) -> failwith "todo" *)
+  | Exp_fun ((pat_hd, pat_tl), exp) ->
+    let* body_anf = transform_expr exp (fun exp_res -> return @@ ACE (CImm exp_res)) in
+    let params =
+      List.fold_right
+        (fun pat acc -> ACE (CFun (get_pattern_name pat, acc)))
+        (pat_hd :: pat_tl)
+        body_anf
+    in
+    let* t = fresh_temp in
+    let* rest = k @@ ImmId t in
+    return @@ ALet (Nonrecursive, t, extract_cexpr params, rest)
   | _ -> failwith "other expression in future versions"
 ;;
 
-(*
-   let rec fac n = if n <= 1 then 1 else n * fac (n - 1)
+(* let rec fac n = if n <= 1 then 1 else n * fac (n - 1)
 
 let main =
   let () = print_int (fac 4) in
