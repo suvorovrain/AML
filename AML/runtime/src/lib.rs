f5b3489955dd731689b1067941124c6112c3fe0b
Dmitri Chirkov <chdmitry2@gmail.com>
2025-10-15 22:10:53 +0300

    add: init runtime
    Signed-off-by: Dmitri Chirkov <chdmitry2@gmail.com>

--- AML/runtime/src/lib.rs
+++ AML/runtime/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn aml_print_int(n: i64) {
+    println!("{}", n);
 }
