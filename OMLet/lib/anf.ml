10647fd8423e3c86bb52d469652f49307f42d98a
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-10-16 18:24:17 +0300

    ref: add cbinop type for better binop treatment

--- OMLet/lib/anf.ml
+++ OMLet/lib/anf.ml
@@ -1,30 +1,31 @@
 open Ast
 
 type immexpr =
-  | ImmNum of int
-  | ImmId of ident
+  | ImmNum of int                     (* 42 *)
+  | ImmId of ident                    (* a *)
+
+type cbinop =
+  | CPlus   (* 42 + a *)
+  | CMinus  (* 42 - a *)
+  | CMul    (* 42 * a *)
+  | CDiv    (* 42 / a *)
+  | CEq     (* 42 = a *)
+  | CNeq    (* 42 != a *)
+  | CLt     (* 42 < a *)
+  | CLte    (* 42 <= a *)
+  | CGt     (* 42 > a *)
+  | CGte    (* 42 >= a *)
 
 type cexpr =
-  | CPlus of immexpr * immexpr
-  | CMinus of immexpr * immexpr
-  | CMul of immexpr * immexpr
-  | CDiv of immexpr * immexpr
-  | CIte of cexpr * aexpr * aexpr
-  | CEq of immexpr * immexpr
-  | CNeq of immexpr * immexpr
-  | CLt of immexpr * immexpr
-  | CLte of immexpr * immexpr
-  | CGt of immexpr * immexpr
-  | CGte of immexpr * immexpr
+  | CBinop of cbinop * immexpr * immexpr
+  | CIte of cexpr * aexpr * aexpr     (* if (42 > a) then 42 else a *)
   | CImmexpr of immexpr
-  | CLam of ident * aexpr
-  | CApp of immexpr * immexpr list
+  | CLam of ident * aexpr             (* fun a -> a + 42 *)
+  | CApp of immexpr * immexpr list    (* func_name arg1 arg2 ... argn *)
 
 and aexpr =
   | ALet of ident * cexpr * aexpr
   | ACExpr of cexpr
-
-type astatement = ALetStatement of is_recursive * (ident * aexpr) list
 
 type aconstruction =
   | AExpr of aexpr
@@ -40,32 +41,32 @@ let gen_temp base =
 ;;
 
 let binop_map = function
-  | Binary_add -> "res_of_plus", fun (l, r) -> CPlus (l, r)
-  | Binary_subtract -> "res_of_minus", fun (l, r) -> CMinus (l, r)
-  | Binary_multiply -> "res_of_mul", fun (l, r) -> CMul (l, r)
-  | Binary_divide -> "res_of_div", fun (l, r) -> CDiv (l, r)
-  | Binary_equal -> "eq", fun (l, r) -> CEq (l, r)
-  | Binary_unequal -> "neq", fun (l, r) -> CNeq (l, r)
-  | Binary_less -> "lt", fun (l, r) -> CLt (l, r)
-  | Binary_less_or_equal -> "lte", fun (l, r) -> CLte (l, r)
-  | Binary_greater -> "gt", fun (l, r) -> CGt (l, r)
-  | Binary_greater_or_equal -> "gte", fun (l, r) -> CGte (l, r)
+  | Binary_add -> "res_of_plus", CPlus
+  | Binary_subtract -> "res_of_minus", CMinus
+  | Binary_multiply -> "res_of_mul", CMul
+  | Binary_divide -> "res_of_div", CDiv
+  | Binary_equal -> "eq", CEq
+  | Binary_unequal -> "neq", CNeq
+  | Binary_less -> "lt", CLt
+  | Binary_less_or_equal -> "lte", CLte
+  | Binary_greater -> "gt", CGt
+  | Binary_greater_or_equal -> "gte", CGte
   | _ -> failwith "NYI"
 ;;
 
 let rec anf (e : expr) (expr_with_hole : immexpr -> aexpr) =
-  let anf_binop opname ctor left right expr_with_hole =
+  let anf_binop opname op left right expr_with_hole =
     let varname = gen_temp opname in
     anf left (fun limm ->
       anf right (fun rimm ->
-        ALet (varname, ctor (limm, rimm), expr_with_hole (ImmId varname))))
+        ALet (varname, CBinop (op, limm, rimm), expr_with_hole (ImmId varname))))
   in
   match e with
   | Const (Int_lt n) -> expr_with_hole (ImmNum n)
   | Variable id -> expr_with_hole (ImmId id)
   | Bin_expr (op, l, r) ->
-    let opname, ctor = binop_map op in
-    anf_binop opname ctor l r expr_with_hole
+    let opname, op_name = binop_map op in
+    anf_binop opname op_name l r expr_with_hole
   | LetIn (_, Let_bind (PVar id, [], expr), [], body) ->
     anf expr (fun immval -> ALet (id, CImmexpr immval, anf body expr_with_hole))
   | LetIn (_, Let_bind (PConst Unit_lt, [], expr), [], body) ->
@@ -100,7 +101,7 @@ let rec anf (e : expr) (expr_with_hole : immexpr -> aexpr) =
       anf args (fun argimm ->
         let name = gen_temp "res_of_apply" in
         ALet (name, CApp (fimm, [ argimm ]), expr_with_hole (ImmId name))))
-  | _ -> failwith "NYI"
+  | _ -> failwith "anf expr NYI"
 ;;
 
 let anf_construction (c : construction) : aconstruction =
@@ -136,7 +137,7 @@ let anf_construction (c : construction) : aconstruction =
     in
     AStatement (flag, [name, ACExpr lambda]) *)
   | Expr e -> AExpr (anf e (fun immval -> ACExpr (CImmexpr immval)))
-  | _ -> failwith "NYI"
+  | _ -> failwith "anf construction NYI"
 ;;
 
 let rec anf_constructions (cs : construction list) : aconstructions =
