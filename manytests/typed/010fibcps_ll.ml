95095b35f169165742e6eec4653555b03dc2aa1f
Kakadu <Kakadu@pm.me>
2025-10-20 12:30:39 +0300

    Task for Deadline 3
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ manytests/typed/010fibcps_ll.ml
@@ -0,0 +1,15 @@
+let id x = x
+let fresh_2 p1 k p2 =
+  k (p1 + p2) in
+
+let fresh_1 n k fib p1 =
+  fib (n-2) (fresh_2 p1 k)
+
+let rec fib n k =
+  if n < 2
+  then k n
+  else fib (n - 1) fresh_1 n k fib
+
+let main =
+  let z = print (fib 6 id)  in
+  0
