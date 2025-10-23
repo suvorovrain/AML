89b1b10aef796590022497e2753a1710f18e76fc
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-23 14:57:35 +0300

    feat: runtime values representation
    This patch brings two structs: Value
    represent any code object in rutime. For
    this moments it's only integers and function.
    Functions are represented by Closure structure.
    It contains pointer at code, arity of function,
    number of applied args and list of applied args.
    
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- AML/runtime/src/lib.rs
+++ AML/runtime/src/lib.rs
@@ -5,3 +5,17 @@ pub extern "C" fn print_int(n: i64) {
     print!("{}", n);
     io::stdout().flush().unwrap()
 }
+
+#[derive (Debug)]
+enum Value {
+    INT(i64),
+    FUN(Box<Closure>)
+    // TODO: other types in future versions
+}
+#[derive (Debug)]
+struct Closure {
+    code: *const (),
+    arity: u8,
+    applied_args_num: u8,
+    applied_args_list: Vec<Value>, 
+}
