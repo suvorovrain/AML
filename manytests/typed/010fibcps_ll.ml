960fd3cf114d000a3dcc377f69fbc617b8bc63a8
sofyak0zyreva <k81sofia@gmail.com>
2025-10-23 16:52:11 +0300

    fix: fib test
    Signed-off-by: sofyak0zyreva <k81sofia@gmail.com>

--- manytests/typed/010fibcps_ll.ml
+++ manytests/typed/010fibcps_ll.ml
@@ -1,6 +1,6 @@
 let id x = x
 let fresh_2 p1 k p2 =
-  k (p1 + p2) in
+  k (p1 + p2)
 
 let fresh_1 n k fib p1 =
   fib (n-2) (fresh_2 p1 k)
@@ -8,7 +8,7 @@ let fresh_1 n k fib p1 =
 let rec fib n k =
   if n < 2
   then k n
-  else fib (n - 1) fresh_1 n k fib
+  else fib (n - 1) (fresh_1 n k fib)
 
 let main =
   let z = print_int (fib 6 id)  in
