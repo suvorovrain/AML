9ea799c9012a0acf5fd196dcfcd07dcd3a606986
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-25 01:22:59 +0300

    feat: runtime for partial applicaton.
    This patch brings runtime that contains closure
    operations logic --- closure_create and
    closure_apply. Runtime was developed by
    `unsafe` and maybe `some cringe` driven development
    methodology.
    
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- AML/runtime/src/lib.rs
+++ AML/runtime/src/lib.rs
@@ -1,97 +1,63 @@
-use std::arch::asm;
 use std::io::{self, Write};
 use std::mem::transmute;
 use std::process::abort;
 
-macro_rules! call_with_args {
-    ($func:expr, $args_ptr:expr, $argc:expr) => {{
-        let res: Value;
+#[repr(C)]
+pub struct Closure {
+    code: *const (),
+    arity: i64,
+    applied: i64,
+    args: Vec<i64>,
+}
+
+macro_rules! call_with_i64_args {
+    ($code:expr, $args:expr, $argc:expr) => {{
+        let res: i64;
         match $argc {
             0 => {
-                let f: extern "C" fn() -> Value = transmute($func);
+                let f: extern "C" fn() -> i64 = transmute($code);
                 res = f();
             }
             1 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let f: extern "C" fn(Value) -> Value = transmute($func);
-                res = f(arg0);
+                let f: extern "C" fn(i64) -> i64 = transmute($code);
+                res = f($args[0]);
             }
             2 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let f: extern "C" fn(Value, Value) -> Value = transmute($func);
-                res = f(arg0, arg1);
+                let f: extern "C" fn(i64, i64) -> i64 = transmute($code);
+                res = f($args[0], $args[1]);
             }
             3 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let arg2 = (*$args_ptr.add(2)).clone();
-                let f: extern "C" fn(Value, Value, Value) -> Value = transmute($func);
-                res = f(arg0, arg1, arg2);
+                let f: extern "C" fn(i64, i64, i64) -> i64 = transmute($code);
+                res = f($args[0], $args[1], $args[2]);
             }
             4 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let arg2 = (*$args_ptr.add(2)).clone();
-                let arg3 = (*$args_ptr.add(3)).clone();
-                let f: extern "C" fn(Value, Value, Value, Value) -> Value = transmute($func);
-                res = f(arg0, arg1, arg2, arg3);
+                let f: extern "C" fn(i64, i64, i64, i64) -> i64 = transmute($code);
+                res = f($args[0], $args[1], $args[2], $args[3]);
             }
             5 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let arg2 = (*$args_ptr.add(2)).clone();
-                let arg3 = (*$args_ptr.add(3)).clone();
-                let arg4 = (*$args_ptr.add(4)).clone();
-                let f: extern "C" fn(Value, Value, Value, Value, Value) -> Value = transmute($func);
-                res = f(arg0, arg1, arg2, arg3, arg4);
+                let f: extern "C" fn(i64, i64, i64, i64, i64) -> i64 = transmute($code);
+                res = f($args[0], $args[1], $args[2], $args[3], $args[4]);
             }
             6 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let arg2 = (*$args_ptr.add(2)).clone();
-                let arg3 = (*$args_ptr.add(3)).clone();
-                let arg4 = (*$args_ptr.add(4)).clone();
-                let arg5 = (*$args_ptr.add(5)).clone();
-                let f: extern "C" fn(Value, Value, Value, Value, Value, Value) -> Value =
-                    transmute($func);
-                res = f(arg0, arg1, arg2, arg3, arg4, arg5);
+                let f: extern "C" fn(i64, i64, i64, i64, i64, i64) -> i64 = transmute($code);
+                res = f($args[0], $args[1], $args[2], $args[3], $args[4], $args[5]);
             }
             7 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let arg2 = (*$args_ptr.add(2)).clone();
-                let arg3 = (*$args_ptr.add(3)).clone();
-                let arg4 = (*$args_ptr.add(4)).clone();
-                let arg5 = (*$args_ptr.add(5)).clone();
-                let arg6 = (*$args_ptr.add(6)).clone();
-                let f: extern "C" fn(Value, Value, Value, Value, Value, Value, Value) -> Value =
-                    transmute($func);
-                res = f(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
+                let f: extern "C" fn(i64, i64, i64, i64, i64, i64, i64) -> i64 = transmute($code);
+                res = f(
+                    $args[0], $args[1], $args[2], $args[3], $args[4], $args[5], $args[6],
+                );
             }
             8 => {
-                let arg0 = (*$args_ptr.add(0)).clone();
-                let arg1 = (*$args_ptr.add(1)).clone();
-                let arg2 = (*$args_ptr.add(2)).clone();
-                let arg3 = (*$args_ptr.add(3)).clone();
-                let arg4 = (*$args_ptr.add(4)).clone();
-                let arg5 = (*$args_ptr.add(5)).clone();
-                let arg6 = (*$args_ptr.add(6)).clone();
-                let arg7 = (*$args_ptr.add(7)).clone();
-                let f: extern "C" fn(
-                    Value,
-                    Value,
-                    Value,
-                    Value,
-                    Value,
-                    Value,
-                    Value,
-                    Value,
-                ) -> Value = transmute($func);
-                res = f(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+                let f: extern "C" fn(i64, i64, i64, i64, i64, i64, i64, i64) -> i64 =
+                    transmute($code);
+                res = f(
+                    $args[0], $args[1], $args[2], $args[3], $args[4], $args[5], $args[6], $args[7],
+                );
             }
             _ => {
-                eprintln!("unsupported arity > 8");
+                // TODO: add support of partial application of functions with more than 8 args
+                eprintln!("fatal: unsupported arity > 8");
                 abort();
             }
         }
@@ -99,122 +65,60 @@ macro_rules! call_with_args {
     }};
 }
 
-// unsafe fn call_with_stack_args(func: *const (), args_ptr: *const Value, argc: usize) ->  Value {
-//     assert!(argc > 8);
-//     let reg_args = std::slice::from_raw_parts(args_ptr, 8);
-//     let stack_args = std::slice::from_raw_parts(args_ptr.add(8), argc - 8);
-//     let stack_bytes = (argc - 8) * std::mem::size_of::<Value>();
-
-//     asm!("addi sp, sp, -{sb}", sb = in(reg) stack_bytes);
-
-//     for (i, val) in stack_args.iter().enumerate() {
-//         let offset = (i * 8) as isize;
-//         asm!(
-//             "sd {val}, {offset}(sp)",
-//             val = in(reg) val,
-//             offset = in(reg) offset,
-//         );
-//     }
-
-//     let res: Value;
-//     let regres:i64;
-//     asm!(
-//         "mv a0, {a0}",
-//         "mv a1, {a1}",
-//         "mv a2, {a2}",
-//         "mv a3, {a3}",
-//         "mv a4, {a4}",
-//         "mv a5, {a5}",
-//         "mv a6, {a6}",
-//         "mv a7, {a7}",
-//         "jalr {func}",
-//         "add sp, sp, {stack_bytes}",
-//         a0 = in(reg) &reg_args[0],
-//         a1 = in(reg) &reg_args[1],
-//         a2 = in(reg) &reg_args[2],
-//         a3 = in(reg) &reg_args[3],
-//         a4 = in(reg) &reg_args[4],
-//         a5 = in(reg) &reg_args[5],
-//         a6 = in(reg) &reg_args[6],
-//         a7 = in(reg) &reg_args[7],
-//         func = in(reg) func,
-//         stack_bytes = in(reg) stack_bytes,
-//         lateout("a0") regres,
-//     );
-//     res = Value::INT(regres);
-//     res
-// }
-
 #[no_mangle]
 pub extern "C" fn print_int(n: i64) {
     print!("{}", n);
-    io::stdout().flush().unwrap()
+    io::stdout().flush().unwrap();
 }
 
-#[derive(Debug, Clone)]
-enum Value {
-    INT(i64),
-    FUN(Box<Closure>), // TODO: other types in future versions
-}
-
-// TODO: try to replace all `*const ()` with smth more safe
-#[derive(Debug, Clone)]
-struct Closure {
-    code: *const (),
-    arity: u8,
-    applied_args_num: u8,
-    applied_args_list: Vec<Value>,
-}
-
-extern "C" fn closure_alloc(func: *const (), arity: u8) -> *mut Closure {
-    let clos: *mut Closure = Box::into_raw(Box::new(Closure {
+#[no_mangle]
+pub extern "C" fn closure_alloc(func: *const (), arity: i64) -> i64 {
+    let clos = Closure {
         code: func,
-        arity: arity,
-        applied_args_num: 0,
-        applied_args_list: Vec::new(),
-    }));
-    return clos;
+        arity,
+        applied: 0,
+        args: Vec::with_capacity(arity as usize),
+    };
+    let ptr = Box::into_raw(Box::new(clos));
+    ptr as i64
 }
 
-extern "C" fn closure_apply(
-    clos_ptr: *mut Closure,
-    passed_args_num: u8,
-    args_ptr: *mut Value,
-) -> Value {
-    let total_arity;
-    let applied_args_num;
-    let args_list;
-    let src_func;
+#[no_mangle]
+pub extern "C" fn closure_apply(clos_raw: i64, argc: i64, argv: *const i64) -> i64 {
     unsafe {
-        src_func = (*clos_ptr).code;
-        total_arity = (*clos_ptr).arity;
-        applied_args_num = (*clos_ptr).applied_args_num;
-        args_list = (*clos_ptr).applied_args_list.clone();
-    };
-    let result;
-    if (applied_args_num + passed_args_num) < total_arity {
-        let clos = closure_alloc(src_func, total_arity);
-        unsafe {
-            (*clos).applied_args_num = applied_args_num + passed_args_num;
-            for v in args_list {
-                (*clos).applied_args_list.push(v.clone());
-            }
-            result = Value::FUN(Box::from_raw(clos));
-            return result;
+        let clos_ptr = clos_raw as *mut Closure;
+        if clos_ptr.is_null() {
+            eprintln!("fatal: closure_apply on null pointer");
+            abort();
         }
-    } else if (applied_args_num + passed_args_num) == total_arity {
-        unsafe {
-            match passed_args_num {
-                0..=8 => call_with_args!(src_func, args_ptr, passed_args_num),
-                _ => call_with_args!(src_func, args_ptr, passed_args_num), // TODO: with stack
-            }
+
+        let src = &*clos_ptr;
+        let total = src.arity as usize;
+        let have = src.applied as usize;
+
+        let mut args = Vec::with_capacity(total);
+        args.extend_from_slice(&src.args);
+        for i in 0..(argc as usize) {
+            let val = *argv.add(i);
+            args.push(val);
         }
-    } else {
-        eprintln!(
-            "fatal: over-application detected (arity = {}, got = {})",
-            total_arity,
-            applied_args_num + passed_args_num
-        );
-        abort();
+
+        let got = have + argc as usize;
+
+        if got < total {
+            // partial application
+            let new_clos = Closure {
+                code: src.code,
+                arity: src.arity,
+                applied: got as i64,
+                args,
+            };
+            Box::into_raw(Box::new(new_clos)) as i64
+        } else if got == total {
+            call_with_i64_args!(src.code, args, total)
+        } else {
+            eprintln!("fatal: over-application (arity={}, got={})", total, got);
+            abort();
+        }
     }
 }
