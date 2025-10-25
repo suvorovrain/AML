9d7381e98dab6823f5ba8caa61f2cc5199184d2d
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-10-25 17:21:07 +0300

    feat: mli file for resultCounter
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

---
+++ OMLet/lib/resultCounter.mli
@@ -0,0 +1,20 @@
+(** Copyright 2025, Ksenia Kotelnikova <xeniia.ka@gmail.com>, Sofya Kozyreva <k81sofia@gmail.com>, Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com> *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+module type ResultCounterMonadType = sig
+    type ('a, 'e) t
+  
+    val return : 'a -> ('a, 'e) t
+    val ( >>= ) : ('a, 'e) t -> ('a -> ('b, 'e) t) -> ('b, 'e) t
+    val fail : 'e -> ('a, 'e) t
+    val read : (int, 'e) t
+    val write : int -> (unit, 'e) t
+    val run : ('a, 'e) t -> int -> ('a * int, 'e) result
+  
+    module Syntax : sig
+      val ( let* ) : ('a, 'e) t -> ('a -> ('b, 'e) t) -> ('b, 'e) t
+    end
+  end
+
+module ResultCounterMonad : ResultCounterMonadType
