932fbd43362e9b819469eb6fee040bf74d928cfd
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-11-03 07:03:58 +0300

    fix: now CC doesn't create top-level aliases
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- AML/lib/middleend/CC.ml
+++ AML/lib/middleend/CC.ml
@@ -2,28 +2,29 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-open Base
 open Anf_types
-module S = Set
-module M = Map
+module SS = Set.Make (String)
+module SM = Map.Make (String)
+
+(* ---------------- State/monad ---------------- *)
 
 type cc_state =
   { temps : int
-  ; bound : (string, String.comparator_witness) S.t
-  ; subst : immexpr M.M(String).t
+  ; bound : SS.t
+  ; subst : immexpr SM.t
   }
 
 module CCState = struct
-  type 'a t = cc_state -> ('a, string) Result.t * cc_state
+  type 'a t = cc_state -> ('a, string) result * cc_state
 
   let return x st = Ok x, st
   let error msg st = Error msg, st
 
-  let bind : 'a t -> ('a -> 'b t) -> 'b t =
-    fun t f st ->
-    match t st with
+  let bind (m : 'a t) (f : 'a -> 'b t) : 'b t =
+    fun st ->
+    match m st with
     | Error msg, st' -> Error msg, st'
-    | Ok a, tran_st -> f a tran_st
+    | Ok a, st' -> f a st'
   ;;
 
   let ( let* ) = bind
@@ -50,153 +51,136 @@ let fresh_name (base : string) : string CCState.t =
   return (Printf.sprintf "%s_cc_%d" base id)
 ;;
 
-let free_vars_imm : immexpr -> (string, String.comparator_witness) S.t = function
-  | ImmNum _ -> S.empty (module String)
-  | ImmId id -> S.singleton (module String) id
+let free_vars_imm = function
+  | ImmNum _ -> SS.empty
+  | ImmId id -> SS.singleton id
 ;;
 
-let rec free_vars_cexpr : cexpr -> (string, String.comparator_witness) S.t = function
+let rec free_vars_cexpr = function
   | CImm imm -> free_vars_imm imm
-  | CBinop (_, i1, i2) -> S.union (free_vars_imm i1) (free_vars_imm i2)
+  | CBinop (_, i1, i2) -> SS.union (free_vars_imm i1) (free_vars_imm i2)
   | CApp (f, args) ->
-    List.fold args ~init:(free_vars_imm f) ~f:(fun acc arg ->
-      S.union acc (free_vars_imm arg))
+    List.fold_left (fun acc a -> SS.union acc (free_vars_imm a)) (free_vars_imm f) args
   | CIte (i, thn, els) ->
-    S.union (free_vars_imm i) (S.union (free_vars_aexpr thn) (free_vars_aexpr els))
-  | CFun (arg, body) -> S.remove (free_vars_aexpr body) arg
+    SS.union (free_vars_imm i) (SS.union (free_vars_aexpr thn) (free_vars_aexpr els))
+  | CFun (arg, body) -> SS.remove arg (free_vars_aexpr body)
 
-and free_vars_aexpr : aexpr -> (string, String.comparator_witness) S.t = function
+and free_vars_aexpr = function
   | ACE cexpr -> free_vars_cexpr cexpr
   | ALet (Nonrecursive, name, rhs, body) ->
     let rhs_fvs = free_vars_cexpr rhs in
-    let body_fvs = S.remove (free_vars_aexpr body) name in
-    S.union rhs_fvs body_fvs
+    let body_fvs = SS.remove name (free_vars_aexpr body) in
+    SS.union rhs_fvs body_fvs
   | ALet (Recursive, name, rhs, body) ->
-    let rhs_fvs = S.remove (free_vars_cexpr rhs) name in
-    let body_fvs = S.remove (free_vars_aexpr body) name in
-    S.union rhs_fvs body_fvs
+    let rhs_fvs = SS.remove name (free_vars_cexpr rhs) in
+    let body_fvs = SS.remove name (free_vars_aexpr body) in
+    SS.union rhs_fvs body_fvs
 ;;
 
-let with_new_scope
-      (new_bound : (string, String.comparator_witness) S.t)
-      (new_subst : immexpr M.M(String).t)
-      (comp : 'a CCState.t)
-  : 'a CCState.t
-  =
+let with_new_scope (new_bound : SS.t) (new_subst : immexpr SM.t) (comp : 'a CCState.t) =
   let* old_st = get in
   let* () = put { old_st with bound = new_bound; subst = new_subst } in
-  let* result = comp in
+  let* res = comp in
   let* new_st = get in
+  (* вернуть temps, чтобы счётчик не терялся *)
   let* () = put { old_st with temps = new_st.temps } in
-  return result
+  return res
 ;;
 
-let with_binding (name : string) (comp : 'a CCState.t) : 'a CCState.t =
-  let* old_st = get in
-  let new_bound = S.add old_st.bound name in
-  with_new_scope new_bound old_st.subst comp
+let with_binding (name : string) (comp : 'a CCState.t) =
+  let* st = get in
+  let new_bound = SS.add name st.bound in
+  with_new_scope new_bound st.subst comp
 ;;
 
-let rec cc_aexpr (aexpr : aexpr) : aexpr CCState.t =
-  match aexpr with
+let rec mk_fun (params : string list) (body : aexpr) : aexpr =
+  match params with
+  | [] -> body
+  | p :: ps -> ACE (CFun (p, mk_fun ps body))
+;;
+
+let rec cc_aexpr = function
   | ACE cexpr -> cc_cexpr_to_aexpr cexpr
   | ALet (rec_flag, name, rhs, body) ->
     (match rhs with
      | CFun (arg, f_body) ->
        let* st = get in
        let fvs = free_vars_cexpr (CFun (arg, f_body)) in
-       let fvs_for_env =
-         match rec_flag with
-         | Recursive -> fvs
-         | Nonrecursive -> fvs
-       in
-       let captured_list = S.to_list (S.inter fvs_for_env st.bound) in
-       let* helper_name = fresh_name name in
-       let inner_bound_vars =
-         S.union
-           (S.of_list (module String) (arg :: captured_list))
-           (match rec_flag with
-            | Recursive -> S.singleton (module String) name
-            | Nonrecursive -> S.empty (module String))
-       in
-       let new_f_body_bound = S.union inner_bound_vars st.bound in
-       let new_subst =
-         match rec_flag with
-         | Recursive -> st.subst
-         | Nonrecursive -> st.subst
-       in
-       let* new_f_body = with_new_scope new_f_body_bound new_subst (cc_aexpr f_body) in
-       let helper_fun_cexpr =
-         match
-           List.fold_right
-             captured_list
-             ~init:(ACE (CFun (arg, new_f_body)))
-             ~f:(fun cap_var acc_aexpr -> ACE (CFun (cap_var, acc_aexpr)))
-         with
-         | ACE c -> c
-         | _ -> assert false
-       in
-       let closure_cexpr =
-         CApp (ImmId helper_name, List.map captured_list ~f:(fun id -> ImmId id))
-       in
-       let* new_let_body = with_binding name (cc_aexpr body) in
-       let helper_item =
-         ALet
-           ( Nonrecursive
-           , helper_name
-           , helper_fun_cexpr
-           , ALet
-               ( (match rec_flag with
-                  | Recursive -> Recursive
-                  | Nonrecursive -> Nonrecursive)
-               , name
-               , closure_cexpr
-               , new_let_body ) )
-       in
-       return helper_item
+       let captured = SS.elements (SS.inter fvs st.bound) in
+       if captured = []
+       then (
+         let inner_bound =
+           match rec_flag with
+           | Recursive -> SS.add arg (SS.add name st.bound)
+           | Nonrecursive -> SS.add arg st.bound
+         in
+         let* new_body = with_new_scope inner_bound st.subst (cc_aexpr f_body) in
+         let* new_body' = with_binding name (cc_aexpr body) in
+         return (ALet (rec_flag, name, CFun (arg, new_body), new_body')))
+       else
+         let* helper = fresh_name name in
+         let helper_params =
+           match rec_flag with
+           | Recursive -> name :: (captured @ [ arg ])
+           | Nonrecursive -> captured @ [ arg ]
+         in
+         let inner_bound =
+           List.fold_left (fun acc p -> SS.add p acc) st.bound helper_params
+         in
+         let* new_body = with_new_scope inner_bound st.subst (cc_aexpr f_body) in
+         let helper_fun = mk_fun helper_params new_body in
+         let helper_cexpr =
+           match helper_fun with
+           | ACE c -> c
+           | _ -> assert false
+         in
+         let clos_args =
+           match rec_flag with
+           | Recursive -> ImmId name :: List.map (fun x -> ImmId x) captured
+           | Nonrecursive -> List.map (fun x -> ImmId x) captured
+         in
+         let closure = CApp (ImmId helper, clos_args) in
+         let* new_body' = with_binding name (cc_aexpr body) in
+         return
+           (ALet
+              ( Nonrecursive
+              , helper
+              , helper_cexpr
+              , ALet (rec_flag, name, closure, new_body') ))
      | _ ->
-       let* new_rhs = cc_cexpr rhs in
-       let* new_body = with_binding name (cc_aexpr body) in
-       return (ALet (rec_flag, name, new_rhs, new_body)))
+       let* rhs' = cc_cexpr rhs in
+       let* body' = with_binding name (cc_aexpr body) in
+       return (ALet (rec_flag, name, rhs', body')))
 
-and cc_cexpr_to_aexpr (cexpr : cexpr) : aexpr CCState.t =
-  match cexpr with
+and cc_cexpr_to_aexpr = function
   | CFun (arg, body) ->
     let* st = get in
     let fvs = free_vars_cexpr (CFun (arg, body)) in
-    let captured_list = S.to_list (S.inter fvs st.bound) in
-    let* helper_name = fresh_name "f" in
-    let* closure_name = fresh_name "closure" in
-    let new_body_bound =
-      S.union st.bound (S.of_list (module String) (arg :: captured_list))
-    in
-    let* new_body = with_new_scope new_body_bound st.subst (cc_aexpr body) in
-    let helper_fun_cexpr =
-      match
-        List.fold_right
-          captured_list
-          ~init:(ACE (CFun (arg, new_body)))
-          ~f:(fun cap_var acc_aexpr -> ACE (CFun (cap_var, acc_aexpr)))
-      with
-      | ACE c -> c
-      | _ -> assert false
-    in
-    let closure_cexpr =
-      CApp (ImmId helper_name, List.map captured_list ~f:(fun id -> ImmId id))
-    in
-    return
-      (ALet
-         ( Nonrecursive
-         , helper_name
-         , helper_fun_cexpr
-         , ALet
-             (Nonrecursive, closure_name, closure_cexpr, ACE (CImm (ImmId closure_name)))
-         ))
+    let captured = SS.elements (SS.inter fvs st.bound) in
+    if captured = []
+    then (
+      let inner = SS.add arg st.bound in
+      let* body' = with_new_scope inner st.subst (cc_aexpr body) in
+      return (ACE (CFun (arg, body'))))
+    else
+      let* helper = fresh_name "f" in
+      let inner =
+        List.fold_left (fun acc p -> SS.add p acc) st.bound (captured @ [ arg ])
+      in
+      let* body' = with_new_scope inner st.subst (cc_aexpr body) in
+      let helper_fun = mk_fun (captured @ [ arg ]) body' in
+      let helper_cexpr =
+        match helper_fun with
+        | ACE c -> c
+        | _ -> assert false
+      in
+      let closure = CApp (ImmId helper, List.map (fun x -> ImmId x) captured) in
+      return (ALet (Nonrecursive, helper, helper_cexpr, ACE closure))
   | CApp (ImmId id, args) ->
     let* st = get in
     let f' =
-      match Map.find st.subst id with
-      | Some f_sub -> f_sub
+      match SM.find_opt id st.subst with
+      | Some v -> v
       | None -> ImmId id
     in
     return (ACE (CApp (f', args)))
@@ -204,17 +188,16 @@ and cc_cexpr_to_aexpr (cexpr : cexpr) : aexpr CCState.t =
   | CImm imm -> return (ACE (CImm imm))
   | CBinop (op, i1, i2) -> return (ACE (CBinop (op, i1, i2)))
   | CIte (i, thn, els) ->
-    let* thn_aexpr = cc_aexpr thn in
-    let* els_aexpr = cc_aexpr els in
-    return (ACE (CIte (i, thn_aexpr, els_aexpr)))
+    let* thn' = cc_aexpr thn in
+    let* els' = cc_aexpr els in
+    return (ACE (CIte (i, thn', els')))
 
-and cc_cexpr (cexpr : cexpr) : cexpr CCState.t =
-  match cexpr with
+and cc_cexpr = function
   | CApp (ImmId id, args) ->
     let* st = get in
     let f' =
-      match Map.find st.subst id with
-      | Some f_sub -> f_sub
+      match SM.find_opt id st.subst with
+      | Some v -> v
       | None -> ImmId id
     in
     return (CApp (f', args))
@@ -222,82 +205,85 @@ and cc_cexpr (cexpr : cexpr) : cexpr CCState.t =
   | CImm imm -> return (CImm imm)
   | CBinop (op, i1, i2) -> return (CBinop (op, i1, i2))
   | CIte (i, thn, els) ->
-    let* thn_aexpr = cc_aexpr thn in
-    let* els_aexpr = cc_aexpr els in
-    return (CIte (i, thn_aexpr, els_aexpr))
+    let* thn' = cc_aexpr thn in
+    let* els' = cc_aexpr els in
+    return (CIte (i, thn', els'))
   | CFun _ -> error "unreachable: cc_cexpr should not encounter CFun"
 ;;
 
-let cc_str_item (item : astructure_item) : astructure_item list CCState.t =
-  match item with
+let cc_str_item = function
   | AStr_eval aexpr ->
-    let* new_aexpr = cc_aexpr aexpr in
-    return [ AStr_eval new_aexpr ]
-  | AStr_value (rec_flag, name, (ACE (CFun (arg, f_body)) as func_aexpr)) ->
-    let* st_before = get in
-    let fvs = free_vars_aexpr func_aexpr in
-    let captured_list = Set.to_list (Set.inter fvs st_before.bound) in
-    let* helper_name = fresh_name name in
-    let helper_params =
-      match rec_flag with
-      | Recursive -> name :: (captured_list @ [ arg ])
-      | Nonrecursive -> captured_list @ [ arg ]
-    in
-    let inner_bound =
-      Set.union st_before.bound (Set.of_list (module String) helper_params)
-    in
-    let* new_f_body = with_new_scope inner_bound st_before.subst (cc_aexpr f_body) in
-    let rec mk_fun params body =
-      match params with
-      | [] -> body
-      | p :: ps -> ACE (CFun (p, mk_fun ps body))
-    in
-    let helper_fun_aexpr = mk_fun helper_params new_f_body in
-    let closure_args =
-      match rec_flag with
-      | Recursive -> ImmId name :: List.map captured_list ~f:(fun id -> ImmId id)
-      | Nonrecursive -> List.map captured_list ~f:(fun id -> ImmId id)
-    in
-    let closure_item =
-      match rec_flag with
-      | Nonrecursive ->
-        let clos = CApp (ImmId helper_name, closure_args) in
-        AStr_value (Nonrecursive, name, ACE clos)
-      | Recursive ->
-        let rhs =
-          ACE (CFun (arg, ACE (CApp (ImmId helper_name, closure_args @ [ ImmId arg ]))))
-        in
-        AStr_value (Recursive, name, rhs)
-    in
-    let helper_item = AStr_value (Nonrecursive, helper_name, helper_fun_aexpr) in
-    let* st_after = get in
-    let new_top = Set.add (Set.add st_before.bound name) helper_name in
-    let* () = put { temps = st_after.temps; bound = new_top; subst = st_before.subst } in
-    return [ helper_item; closure_item ]
+    let* a' = cc_aexpr aexpr in
+    return [ AStr_eval a' ]
+  | AStr_value (rec_flag, name, (ACE (CFun (arg, f_body)) as func)) ->
+    let* st = get in
+    let fvs = free_vars_aexpr func in
+    let captured = SS.elements (SS.inter fvs st.bound) in
+    if captured = []
+    then (
+      let inner =
+        match rec_flag with
+        | Recursive -> SS.add arg (SS.add name st.bound)
+        | Nonrecursive -> SS.add arg st.bound
+      in
+      let* body' = with_new_scope inner st.subst (cc_aexpr f_body) in
+      let* st' = get in
+      let new_top = SS.add name st.bound in
+      let* () = put { temps = st'.temps; bound = new_top; subst = st.subst } in
+      return [ AStr_value (rec_flag, name, ACE (CFun (arg, body'))) ])
+    else
+      let* helper = fresh_name name in
+      let helper_params =
+        match rec_flag with
+        | Recursive -> name :: (captured @ [ arg ])
+        | Nonrecursive -> captured @ [ arg ]
+      in
+      let inner = List.fold_left (fun acc p -> SS.add p acc) st.bound helper_params in
+      let* body' = with_new_scope inner st.subst (cc_aexpr f_body) in
+      let helper_fun = mk_fun helper_params body' in
+      let helper_item =
+        match helper_fun with
+        | ACE c -> AStr_value (Nonrecursive, helper, ACE c)
+        | _ -> assert false
+      in
+      let closure_item =
+        match rec_flag with
+        | Nonrecursive ->
+          let clos = CApp (ImmId helper, List.map (fun x -> ImmId x) captured) in
+          AStr_value (Nonrecursive, name, ACE clos)
+        | Recursive ->
+          let clos_args = ImmId name :: List.map (fun x -> ImmId x) captured in
+          let wrapper =
+            ACE (CFun (arg, ACE (CApp (ImmId helper, clos_args @ [ ImmId arg ]))))
+          in
+          AStr_value (Recursive, name, wrapper)
+      in
+      let* st' = get in
+      let new_top = SS.add helper (SS.add name st.bound) in
+      let* () = put { temps = st'.temps; bound = new_top; subst = st.subst } in
+      return [ helper_item; closure_item ]
   | AStr_value (rec_flag, name, aexpr) ->
-    let* st_before = get in
-    let new_bound_for_body =
+    let* st = get in
+    let inner =
       match rec_flag with
-      | Recursive -> Set.add st_before.bound name
-      | Nonrecursive -> st_before.bound
+      | Recursive -> SS.add name st.bound
+      | Nonrecursive -> st.bound
     in
-    let* new_aexpr = with_new_scope new_bound_for_body st_before.subst (cc_aexpr aexpr) in
-    let* st_after = get in
-    let new_top = Set.add st_before.bound name in
-    let* () = put { temps = st_after.temps; bound = new_top; subst = st_before.subst } in
-    return [ AStr_value (rec_flag, name, new_aexpr) ]
+    let* a' = with_new_scope inner st.subst (cc_aexpr aexpr) in
+    let* st' = get in
+    let new_top = SS.add name st.bound in
+    let* () = put { temps = st'.temps; bound = new_top; subst = st.subst } in
+    return [ AStr_value (rec_flag, name, a') ]
 ;;
 
 let cc_program (prog : aprogram) : aprogram CCState.t =
-  let* lists_of_items = map_m cc_str_item prog in
-  return (List.concat lists_of_items)
+  let* lists = CCState.map_m cc_str_item prog in
+  return (List.concat lists)
 ;;
 
-let cc_transform (prog : aprogram) : (aprogram, string) Result.t =
-  let initial_state =
-    { temps = 0; bound = S.empty (module String); subst = M.empty (module String) }
-  in
-  match CCState.run (cc_program prog) initial_state with
-  | Ok final_prog, _final_state -> Ok final_prog
-  | Error msg, _final_state -> Error msg
+let cc_transform (prog : aprogram) : (aprogram, string) result =
+  let initial = { temps = 0; bound = SS.empty; subst = SM.empty } in
+  match CCState.run (cc_program prog) initial with
+  | Ok p, _ -> Ok p
+  | Error e, _ -> Error e
 ;;
