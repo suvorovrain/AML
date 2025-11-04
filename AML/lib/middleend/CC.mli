e907ac28d0b524ce302465c889dc6cf243f96b7b
Dmitri Chirkov <chdmitry2@gmail.com>
2025-11-04 14:48:23 +0300

    add: mli
    Signed-off-by: Dmitri Chirkov <chdmitry2@gmail.com>

---
+++ AML/lib/middleend/CC.mli
@@ -0,0 +1,7 @@
+(** Copyright 2025-2026, Rodion Suvorov, Dmitriy Chirkov*)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Anf_types
+
+val cc_transform : aprogram -> (aprogram, string) result
