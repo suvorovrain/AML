681bdf08562ca3a2a01a1a5f20be77bcda7a2384
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-11-05 21:21:07 +0300

    feat: comment in treeAnalysis
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- OMLet/lib/treeAnalysis.mli
+++ OMLet/lib/treeAnalysis.mli
@@ -6,6 +6,8 @@ open Anf
 
 val lambda_arity_of_aexpr : aexpr -> int * aexpr
 val is_function : aexpr -> bool
+
+(* currently performs only the calculating of sufficient stack size, but can be used later for implementing other pre-codegen analysis *)
 val analyze_aconstr : int -> aconstruction -> int
 val analyze_astatement : int -> 'a * aexpr -> int
 val analyze_aexpr : int -> aexpr -> int
